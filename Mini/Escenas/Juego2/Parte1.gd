212e2be26a38ceeb1570eb6cc03758cc659d61e0
joelyo1996 <38585894+joelyo1996@users.noreply.github.com>
2022-07-01 10:27:18 -0300

    inicio
    creacion del nivel2

---
+++ Mini/Escenas/Juego2/Parte1.gd
@@ -0,0 +1,30 @@
+extends Sprite
+
+var click = false
+var mauseIn = true
+var a = true
+
+func _process(delta):
+	if click == true:
+		if a == true :
+			if (mauseIn == true && Input.is_action_pressed("left_click")):
+				set_position(get_viewport().get_mouse_position())
+	pass
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if Input.is_action_pressed("left_click"):
+		click = true
+	pass
+	if Input.is_action_just_released("left_click"):
+		click = false
+	pass 
+
+
+func _on_AreaPosicion1_area_entered(area):
+	if area.name == "Area2D":
+		a = false
+		position= Vector2(720,250)
+		var mauseIn = false
+	pass 
+
+
