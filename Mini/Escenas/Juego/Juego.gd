d4bac5135bee8033938808814d50eeda9b36e0e8
joelyo1996 <38585894+joelyo1996@users.noreply.github.com>
2022-07-05 10:53:41 -0300

    tiempo
    programacion de tiempo

--- Mini/Escenas/Juego/Juego.gd
+++ Mini/Escenas/Juego/Juego.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+var t =0
+
 func _process(delta):
 	if Global.victoria == 9:
 		get_tree().change_scene("res://Escenas/Juego2/Juego2.tscn")
+
+
+func _on_Timer_timeout():
+	t = t + 1
+	$Tiempo.text = String(t)
+	pass # Replace with function body.
