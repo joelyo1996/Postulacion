2b6264ffa4cad6262aadb29daf9b5491b5081401
joelyo1996 <38585894+joelyo1996@users.noreply.github.com>
2022-07-05 13:08:10 -0300

    record
    programacion del record

--- Mini/Escenas/Final/Tiempo.gd
+++ Mini/Escenas/Final/Tiempo.gd
@@ -5,4 +5,8 @@ func _ready():
 
 
 func _on_BotonMenu_pressed():
+	Global.victoria = 0
+	if Global.t < Global.segundosrecord:
+		Global.segundosrecord = Global.t
 	get_tree().change_scene("res://Escenas/Menu/Menu.tscn")
+	
--- Mini/Global.gd
+++ Mini/Global.gd
@@ -3,3 +3,5 @@ extends Node
 var victoria = 0
 var t = 0
 var minutos = 0
+var minutosrecord = 0
+var segundosrecord = 60
